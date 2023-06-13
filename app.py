0d7e1dff8cca356a96c9f902df3a5a214fa00a30
Asraa112 <128457943+Asraa112@users.noreply.github.com>
2023-06-13 21:51:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ if st.button("Submit"):
     input_index = 'index.json'
     index = GPTSimpleVectorIndex.load_from_disk(input_index)
     response = index.query(query, response_mode="compact")
-    st.text_input(response_str)
+    response_str=str(response)
+   
